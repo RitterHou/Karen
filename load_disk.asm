635866be7c390c974ff2b49b68c8e2d5185c5ede
Derobukal <hourui93@163.com>
2017-01-17 00:01:53 +0800

    :memo:disk load success, completing print_hex

--- load_disk.asm
+++ load_disk.asm
@@ -1,21 +1,23 @@
 [org 0x7c00]
-mov ah, 0x0e
 mov [BOOT_DRIVE], dl ; BIOS stores our boot drive in DL, so it’s best to remember this for later.
 
 mov bp, 0x8000 ; Here we set our stack safely out of the
 mov sp, bp ; way, at 0x8000
 
-mov bx, 0x9000 ; Load 5 sectors to 0x0000(ES):0x9000(BX)
-mov dh, 5 ; from the boot disk.
+mov bx, 0x9000 ; Load 2 sectors to 0x0000(ES):0x9000(BX)
+mov dh, 2 ; from the boot disk. read 2 sectors from disk.
 mov dl, [BOOT_DRIVE]
+
 call disk_load
+
+mov bx, BOOT
+call print_string
+
 mov dx, [0x9000] ; Print out the first loaded word, which
 call print_hex ; we expect to be 0xdada , stored
 ; at address 0x9000
 mov dx, [0x9000 + 512] ; Also, print the first word from the
 call print_hex ; 2nd loaded sector: should be 0xface
-mov bx, BOOT
-call print_string
 jmp $
 
 ; load DH sectors to ES:BX from drive DL
@@ -32,7 +34,7 @@ disk_load:
     jc disk_error ; Jump if error (i.e. carry flag set)
     pop dx ; Restore DX from the stack
     cmp dh, al ; if AL (sectors read) != DH (sectors expected)
-    jne disk_error ; display error message
+    jne disk_error1 ; display error message
     ret
 
 disk_error:
@@ -40,9 +42,16 @@ disk_error:
     call print_string
     jmp $
 
+disk_error1:
+    mov bx, DISK_ERROR_MSG1
+    call print_string
+    jmp $
+
 DISK_ERROR_MSG db "Disk read error!", 0
+DISK_ERROR_MSG1 db "Disk read error1!", 0
 
 print_string:
+    mov ah, 0x0e
     mov cl, [bx]        ; the value of bx is STRING, so [bx] is the value which in address STRING
     cmp cl, 0           ; cmp cl with 0
     jne bx_add          ; if cl not equals 0, jmp to bx_add(until [bx] is 0, this instruction is running)
@@ -54,32 +63,38 @@ bx_add:
     jmp print_string
 
 print_hex:
+    cmp dx, 0
+    je  end
+
     mov cx, dx
-    and cx, 0xf
-    cmp cx, 10
+    shr dx, 4
+    and cl, 0xf
+    cmp cl, 10
     jl less_ten
-    cmp cx, 10
+    cmp cl, 10
     jge great_ten
+
 end:
     ret
-less_ten:
-    add cx, 30
-    mov ax, cx
-    mov ah, 0x0e
-    int 0x10
-    jmp end
-great_ten:
-    add cx, 55
-    mov ax, cx
-    mov ah, 0x0e
-    int 0x10
-    jmp end
 
-; Global variables
-BOOT_DRIVE: db 0
+less_ten:
+    add cl, 30
+    mov al, cl
+    mov ah, 0x0e
+    int 0x10
+    jmp print_hex
+great_ten:
+    add cl, 55
+    mov al, cl
+    mov ah, 0x0e
+    int 0x10
+    jmp print_hex
 
 BOOT:
     db 'Karen is booting...', 0
+
+; Global variables
+BOOT_DRIVE: db 0
 
 ; Bootsector padding
 times 510-($-$$) db 0
@@ -89,5 +104,5 @@ dw 0xaa55
 ; so if we purposely add a few more sectors to our code by repeating some
 ; familiar numbers , we can prove to ourselfs that we actually loaded those
 ; additional two sectors from the disk we booted from.
-times 256 dw 0xdada
+times 256 dw 0x1234
 times 256 dw 0xface
